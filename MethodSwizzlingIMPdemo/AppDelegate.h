fda4fb67b9f936df8dd45a507a9e99cb53ac3ce4
Mr.Xia <26207005@qq.com>
2016-05-12 14:38:12 +0800

    Initial Commit

---
+++ MethodSwizzlingIMPdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MethodSwizzlingIMPdemo
+//
+//  Created by 帝炎魔 on 16/5/12.
+//  Copyright © 2016年 帝炎魔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MethodSwizzlingIMPdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MethodSwizzlingIMPdemo
+//
+//  Created by 帝炎魔 on 16/5/12.
+//  Copyright © 2016年 帝炎魔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
